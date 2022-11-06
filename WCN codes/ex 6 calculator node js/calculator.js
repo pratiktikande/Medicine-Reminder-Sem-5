269d7e733223cfbc3da35bc58db2751189495bc2
unknown <pratiktikande12@gmail.com>
2022-11-07 01:19:56 +0530

    third

---
+++ WCN codes/ex 6 calculator node js/calculator.js
@@ -0,0 +1,25 @@
+const argvs = process.argv
+const argv = argvs.slice(2)
+const operation = argv[0]
+const operator1 = parseInt(argv[1])
+const operator2 = parseInt(argv[2])
+
+if (operation === 'add') {
+	console.log(operation + ' is '
+		+ (operator1 + operator2));
+}
+if (operation === 'subtract') {
+	console.log(operation + ' is '
+		+ (operator1 - operator2));
+}
+if (operation === 'multiply') {
+	console.log(operation + ' is '
+		+ (operator1 + operator2));
+}
+if (operation === 'divide') {
+	console.log(operation + ' is '
+		+ (operator1 - operator2));
+}
+
+// node app.js add 4 5
+// node app.js subtract 10 5
