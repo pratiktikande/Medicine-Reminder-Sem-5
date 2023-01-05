976663e04f74219a54707a542f0262f39aadb5d9
Pratik Tikande <105018505+pratiktikande@users.noreply.github.com>
2023-01-05 19:03:09 +0530

    Delete WCN codes directory

--- WCN codes/Codes For Exam/index.js
+++
@@ -1,9 +0,0 @@
-const btn = document.getElementById('btn');
-
-btn.addEventListener('click', function onClick(event) {
-  // 👇️ change background color
-  document.body.style.backgroundColor = 'salmon';
-
-  // 👇️ optionally change text color
-  // document.body.style.color = 'white';
-});
